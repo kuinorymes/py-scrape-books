a6364d16250e5b13bf9142ba1b7387e2e7cf9226
Arsen Myroniuk <arisenmyr@gmail.com>
2025-03-31 15:50:41 +0300

    Configured BooksPipeline

--- books/pipelines.py
+++ books/pipelines.py
@@ -2,12 +2,21 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
+import json
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
 
 class BooksPipeline:
+    def open_spider(self, spider):
+        self.file = open("books.jl", "w", encoding="utf-8")
+
+    def close_spider(self, spider):
+        self.file.close()
+
     def process_item(self, item, spider):
+        line = json.dumps(dict(item), ensure_ascii=False) + "\n"
+        self.file.write(line)
+
         return item
